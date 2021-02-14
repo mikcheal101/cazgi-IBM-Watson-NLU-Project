75125ba6f51fb0aa97663b9d670fb805d79b2451
Hirekaan Micheal Hemen <mikcheal101@users.noreply.github.com>
2021-02-15 00:59:27 +0100

    Update EmotionTable.js

--- sentimentAnalyzeClient/src/EmotionTable.js
+++ sentimentAnalyzeClient/src/EmotionTable.js
@@ -2,21 +2,37 @@ import React from 'react';
 import './bootstrap.min.css';
 
 class EmotionTable extends React.Component {
-    render() {
-      return (  
-        <div>
-          {/*You can remove this line and the line below. */}
-          {JSON.stringify(this.props.emotions)}
-          <table className="table table-bordered">
-            <tbody>
-            {
-                //Write code to use the .map method that you worked on in the Hands-on React lab to extract the emotions
-            }
-            </tbody>
-          </table>
-          </div>
-          );
-        }
-    
+
+  state = {
+    items: []
+  };
+
+  constructor(props) {
+    super(props);
+    console.log("loaded");
+  }
+
+  componentDidMount() {
+    // console.log(this.props.emotions.map(item => item));
+    // console.log(this.state);
+    for (var item in this.props.emotions)
+      this.state.items.push(<tr><td>{item}</td><td>{this.props.emotions[item]}</td></tr>);
+    console.log(this.state.items);
+  }
+
+  render() {
+    return (
+      <div>
+        {/*You can remove this line and the line below. */}
+        {/* {JSON.stringify(this.props.emotions)} */}
+        <table className="table table-bordered">
+          <tbody>
+            {(this.state.items.map(row => row))}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+
 }
 export default EmotionTable;
